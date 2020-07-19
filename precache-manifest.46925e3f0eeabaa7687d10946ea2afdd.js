5149e6fb32a9feb28fa2b91c5b6a9d0dbafbece5
AnyThony <28424838+AnyThony@users.noreply.github.com>
2020-07-19 10:34:28 -0400

    build

---
+++ precache-manifest.46925e3f0eeabaa7687d10946ea2afdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "738d1f7b4c54f0845f177cc1ccb32571",
+    "url": "/commerce-dashboard/index.html"
+  },
+  {
+    "revision": "49907faecfb66062c893",
+    "url": "/commerce-dashboard/static/css/main.23d4d462.chunk.css"
+  },
+  {
+    "revision": "31091513e59422475f81",
+    "url": "/commerce-dashboard/static/js/2.ad8122c5.chunk.js"
+  },
+  {
+    "revision": "49907faecfb66062c893",
+    "url": "/commerce-dashboard/static/js/main.3eb73339.chunk.js"
+  },
+  {
+    "revision": "069bffb8e2579aaf09e3",
+    "url": "/commerce-dashboard/static/js/runtime~main.f9490c33.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8ea7bc69a09a93eb38bdff4ce69299da.js"
+  "/commerce-dashboard/precache-manifest.46925e3f0eeabaa7687d10946ea2afdd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/commerce-dashboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
